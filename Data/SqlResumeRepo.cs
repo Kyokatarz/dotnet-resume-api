d56aa73d4928d6b8f85c4d69e820e68e7649e09b
Giang Tran <51933605+Kyokatarz@users.noreply.github.com>
2021-05-14 12:04:43 +0300

    Implement the Sql context repo

--- Data/SqlResumeRepo.cs
+++ Data/SqlResumeRepo.cs
@@ -1,18 +1,25 @@
 using System.Collections.Generic;
+using System.Linq;
 using DotnetResume.Models;
 
 namespace DotnetResume.Data
 {
   public class SqlResumeRepo : IResumeRepo
   {
+    private readonly ResumeContext _context;
+
+    public SqlResumeRepo(ResumeContext context)
+    {
+      _context = context;
+    }
     public IEnumerable<Resume> GetAllResumes()
     {
-      throw new System.NotImplementedException();
+      return _context.Resumes.ToList();
     }
 
     public Resume GetResumeById(int id)
     {
-      throw new System.NotImplementedException();
+      return _context.Resumes.FirstOrDefault(p => p.ResumeId == id);
     }
   }
 }
